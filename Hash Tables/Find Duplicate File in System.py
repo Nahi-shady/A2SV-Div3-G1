5a204464c63eaf67db68799ae8f6742c44efbae4
Nahom Zewdu <nahiyo86@gmail.com>
2023-05-31 14:56:56 +0300

    Solved Find Duplicate Files In Systme

---
+++ Hash Tables/Find Duplicate File in System.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        dic = collections.defaultdict(list)
+        for path in paths:
+            directory, *files = path.split(' ')
+            for file in files:
+                name, content = file.split('(')
+                dic[content].append(directory + '/' + name)
+
+        return [i for i in dic.values() if len(i)>1]
