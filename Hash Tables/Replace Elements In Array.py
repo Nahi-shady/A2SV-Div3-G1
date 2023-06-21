e3be4b83b86a1e26d6bb5a8831ccfc28ff14a8bc
Nahom Zewdu <nahiyo86@gmail.com>
2023-06-21 17:30:17 +0800

    solved Replace Elements In Array

---
+++ Hash Tables/Replace Elements In Array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        mp = {}
+        for x, y in reversed(operations): mp[x] = mp.get(y, y)
+        return [mp.get(x, x) for x in nums]
